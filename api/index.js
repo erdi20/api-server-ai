9318619d57222e6a402433a68178fe5260c67e12
Erdi Masyudi <rayadavid158@gmail.com>
2026-01-10 11:08:55 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,9 +10,10 @@ export default function handler(req, res) {
   }
 
   res.status(200).json({
-    api_url: "https://5f4c63a556fe.ngrok-free.app",
+    api_url: "https://805c70cc892a.ngrok-free.app",
   });
 }
+
 
 
 
